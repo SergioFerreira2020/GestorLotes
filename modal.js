1a1aeec5d94f54f565ff1665c1d8acb1bc6412fd
SergioFerreira2020 <silvasergio49@gmail.com>
2025-11-22 20:27:30 +0000

    modal

--- modal.js
+++ modal.js
@@ -47,3 +47,62 @@ document.getElementById("modalOverlay").addEventListener("click", e => {
 });
 
 document.getElementById("modalClose").addEventListener("click", closeModal);
+
+
+
+/** INFO MODAL **/
+export function openInfoModal(title, htmlContent) {
+    document.getElementById("modalTitle").textContent = title;
+    document.getElementById("modalContent").innerHTML = htmlContent;
+
+    document.getElementById("modalOverlay").classList.remove("hidden");
+}
+
+export function closeInfoModal() {
+    document.getElementById("modalOverlay").classList.add("hidden");
+}
+
+document.getElementById("modalOverlay").addEventListener("click", e => {
+    if (e.target.id === "modalOverlay") closeInfoModal();
+});
+document.getElementById("modalClose").addEventListener("click", closeInfoModal);
+
+
+
+
+/** CONFIRMATION MODAL **/
+export function openConfirmModal(title, messageHtml, onConfirm) {
+
+    document.getElementById("confirmTitle").textContent = title;
+    document.getElementById("confirmMessage").innerHTML = messageHtml;
+
+    const okBtn = document.getElementById("confirmOk");
+    const cancelBtn = document.getElementById("confirmCancel");
+
+    // Remove old listeners
+    okBtn.replaceWith(okBtn.cloneNode(true));
+    cancelBtn.replaceWith(cancelBtn.cloneNode(true));
+
+    // Re-select new clean buttons
+    const newOk = document.getElementById("confirmOk");
+    const newCancel = document.getElementById("confirmCancel");
+
+    // Bind actions
+    newOk.addEventListener("click", () => {
+        closeConfirmModal();
+        onConfirm();
+    });
+
+    newCancel.addEventListener("click", closeConfirmModal);
+
+    // Show modal
+    document.getElementById("confirmOverlay").classList.remove("hidden");
+}
+
+export function closeConfirmModal() {
+    document.getElementById("confirmOverlay").classList.add("hidden");
+}
+
+document.getElementById("confirmOverlay").addEventListener("click", e => {
+    if (e.target.id === "confirmOverlay") closeConfirmModal();
+});
